e538628afa7b568f90491da1c4a79fb74c63911b
liamhuang <liamhuang@liamhuangdeMacBook-Air.local>
2016-10-30 14:47:58 +0800

    增加mp

---
+++ mp/routes/weixin.js
@@ -0,0 +1,17 @@
+/**
+ *  weixin.js 
+ *  微信程序应用
+ *  author liamhuang
+ * 
+ * ******/
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/weixin/auth', function(req, res, next) {
+    if( req && req.echostr ){
+       res.send( req.echostr );
+    }
+});
+
+module.exports = router;
