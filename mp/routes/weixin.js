30f0acf7941a4fea590582019c21be619d09d570
liamhuang <liamhuang@liamhuangdeMacBook-Air.local>
2016-11-02 16:13:44 +0800

    增加了数据

--- mp/routes/weixin.js
+++ mp/routes/weixin.js
@@ -7,18 +7,26 @@
 var express   = require('express');
 var router    = express.Router();
 var Crypto    = require("crypto");
+var xml2js    = require("xml2js");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     req       = req ||{};
     req.query = req.query ||{};
-    console.log( JSON.stringify( req.query ) );
+    req.body  = req.body ||"";   //这里的body都是通过xml的方式传递的，所以需要设置一下。
 
     var signature = req.query.signature || "";  //校验的token
     var timestamp = req.query.timestamp || "";  //校验的token
     var echostr   = req.query.echostr   || "";  //随机字符
     var nonce     = req.query.nonce     || "";  //随机数
     var token     = "8674521";      //token
+    var body      = {};
+    
+    if( req.body ){
+        body = xml2js.parseString( req.body );
+    }
+
+    console.log( JSON.stringify( body ));
 
     if( signature && timestamp && echostr && nonce){
         var arr = [ token , timestamp , nonce];
@@ -29,10 +37,7 @@ router.get('/', function(req, res, next) {
 
         var str         = arr.join("");
         var caculateStr = Crypto.createHash("sha1").update( str ).digest("hex");
-
-        console.log( arr.join("") + "\n");
-        console.log( "caculateStr:" + caculateStr +"\n");
-        console.log( "signature:"   + signature +"\n");
+        
         if( caculateStr == signature ){
             res.send( req.query.echostr );
         }else{
