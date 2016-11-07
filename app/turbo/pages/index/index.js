c5894faf04237fd8fdba579a17b260d2d0b8a2a2
liamhuang <liamhuang@liamhuangdeMacBook-Air.local>
2016-11-07 22:43:35 +0800

    增加了文件

---
+++ app/turbo/pages/index/index.js
@@ -0,0 +1,60 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    list: [] ,
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    
+  },
+  getTurboList:function( cb ){
+    var data = {"target": 700, "type":0};
+
+    var url  = "https://app.liamhuang.com?action=getTurboList"; 
+    wx.request( {
+      "url": url,
+      "data" : data,
+      "method" : "get",
+      "success": function( result ){
+          var list = [];
+          if( 0 == ret.code && ret.data && ret.data.length ){
+              for( var i = 0 ; i < ret.data.length  ; i ++){
+                var cur = ret.data[i];
+                
+                if( cur && cur.code ){
+                  cur.targetPrice = targetPrice;
+                  cur.matchPrice  = Math.floor(cur.exchangePrice*1 + 100*cur.curPrice);
+                  cur.name        = decodeURIComponent(cur.name);
+                  var temShare    = (cur.exchangePrice + cur.curPrice*cur.exchange)/cur.targetPrice - 1;
+                  cur.share       = Math.floor(temShare*1000)/1000;    
+                  list.push( cur );
+                }
+              }
+              cb && cb( list );
+          }
+      },
+      "fail":function(){
+          cb && cb( [] );
+      }
+    })
+    
+  },
+  onLoad: function () {
+    
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+        //更新数据
+        that.setData({
+          userInfo:userInfo
+        });
+
+        that.getTurboList( function( list ){   //获取数据列表，用于展示
+            that.setData( { "list": list })  
+        });
+    })
+  }
+})
--- mp/routes/app.js
+++ mp/routes/app.js
@@ -8,18 +8,49 @@ var express   = require('express');
 var router    = express.Router();
 var Crypto    = require("crypto");
 var xml2js    = require("xml2js");
+var mysql     = require("../model/mysql_server");
 
 /* GET users listing. */
 router.all('/', function(req, res, next) {
     req       = req ||{};
-    req.query = req.query ||{};
-    req.body  = req.body ||"";   //这里的body都是通过xml的方式传递的，所以需要设置一下。
+    var query = req.query ||{};
+    var body  = req.body ||"";   //这里的body都是通过xml的方式传递的，所以需要设置一下。
 
     var signature = req.query.signature || "";  //校验的token
     var timestamp = req.query.timestamp || "";  //校验的token
     var echostr   = req.query.echostr   || "";  //随机字符
 
-    res.send( {data:{"test":"fdasfdsa"}} );
+    console.log( req.url );
+    if( "getTurboList" == query.action ){   //这里进一步的区分数据的类型
+        //这里需要从mysql中获取数据，然后返回给前提
+        mysql.getAllTurbo( query.code ,query.type , function( ret,rows ){
+            if( 0 == ret ){
+				if( rows && rows.length > 0 ){         //如果获取了数据，就直接展示出来
+
+					var data = [];
+					for( var i = 0 ; i < rows.length ; i ++){
+						
+						var cur = rows[i];
+						
+						if( cur ){
+							data.push( cur );
+						}
+					}
+					
+					res.send( data );
+				}else{               //如果获取了数量为0 ， 则展示对映错误
+					res.send( [] );
+				}
+			}else{
+				res.send( [] );
+			}
+        } );
+
+    }else{
+        res.send( {data:{"test":"fdasfdsa"}} );
+    }
+   
+    
 });
 
 module.exports = router;
